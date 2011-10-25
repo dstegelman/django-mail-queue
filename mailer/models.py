b82baa30380ce87044dcfe89a5f97596f7a8c1ec
Derek Stegelman <dstegelman@gmail.com>
2011-10-24 20:29:05 -0500

    UPdates from code review

--- mailer/models.py
+++ mailer/models.py
@@ -12,6 +12,8 @@
 #---------------------------------------------#
 from django.db import models
 from django.core.mail import *
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class MailerMessage(models.Model):
     subject = models.CharField(max_length=250, blank=True, null=True)
@@ -24,6 +26,7 @@ class MailerMessage(models.Model):
     def __unicode__(self):
         return self.subject
     
+    
     def send(self):
         try:
             msg = EmailMessage(self.subject, self.content, self.from_address, [self.to_address])
@@ -33,3 +36,11 @@ class MailerMessage(models.Model):
         except:
             self.sent = False
         self.save()
+        
+    
+@receiver(post_save, sender=MailerMessage)
+def send_reciever(sender, **kwargs):
+    emails = MailerMessage.objects.filter(sent=False)[:30]
+    for email in emails:
+        email.send()
+    
--- mailer/views.py
+++ mailer/views.py
@@ -2,7 +2,7 @@ from mailer.models import MailerMessage
 from django.http import HttpResponse
 
 def run_mail_job(request):
-    emails = MailerMessage.objects.filter(sent=False)
+    emails = MailerMessage.objects.filter(sent=False)[:30]
     for email in emails:
         email.send()
     response = HttpResponse()
