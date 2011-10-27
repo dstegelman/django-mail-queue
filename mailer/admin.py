5714d63eb1d64505c313cd700c6a6299e318f997
Derek Stegelman <dstegelman@gmail.com>
2011-10-27 10:48:12 -0500

    name change.

--- mailer/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from mailer.models import MailerMessage
-
-class MailerAdmin(admin.ModelAdmin):
-    list_display = ('app', 'subject', 'to_address', 'sent')
-
-
-admin.site.register(MailerMessage, MailerAdmin)
--- mailer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-from mailer.views import *
-
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', run_mail_job, name='home'),
-
-)
