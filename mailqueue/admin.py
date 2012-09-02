79168b71dd69486763fd5e75bb2f92b78a3eeb59
Derek Stegelman <dstegelman@gmail.com>
2012-09-02 09:46:25 -0500

    Adding search to mail admin.

--- mailqueue/admin.py
+++ mailqueue/admin.py
@@ -3,15 +3,13 @@ from mailqueue.models import MailerMessage
 
 class MailerAdmin(admin.ModelAdmin):
     list_display = ('app', 'subject', 'to_address', 'sent')
-    
+    search_fields = ['to_address']
     actions = ['send_failed']
-
 
     def send_failed(self, request, queryset):
         emails = queryset.filter(sent=False)
         for email in emails:
             email.send()
         self.message_user(request, "Emails sent.")
-
 
 admin.site.register(MailerMessage, MailerAdmin)
