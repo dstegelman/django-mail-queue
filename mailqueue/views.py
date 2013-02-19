bcdd01d7f083f3dc7a4a617ca84242fb4a84c62e
Rob Speed <rspeed@bounteo.us>
2013-02-19 15:35:36 -0500

    Added support for MAILQUEUE_LIMIT to the run_mail_job view.

--- mailqueue/views.py
+++ mailqueue/views.py
@@ -1,8 +1,12 @@
-from mailqueue.models import MailerMessage
 from django.http import HttpResponse
+from django.conf import settings
+
+from mailqueue.models import MailerMessage
+from mailqueue import defaults
 
 def run_mail_job(request):
-    emails = MailerMessage.objects.filter(sent=False)[:30]
+    limit = getattr(settings, 'MAILQUEUE_LIMIT', defaults.MAILQUEUE_LIMIT)
+    emails = MailerMessage.objects.filter(sent=False)[:limit]
     for email in emails:
         email.send()
     response = HttpResponse()
