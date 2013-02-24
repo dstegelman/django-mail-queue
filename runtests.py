8253b5e580380b77f3a856b8cd3a0ba21d80b3c5
Derek Stegelman <dstegelman@gmail.com>
2013-02-24 16:11:05 -0600

    Fix for tests.

--- runtests.py
+++ runtests.py
@@ -1,21 +1,43 @@
 __author__ = 'derek'
 import os, sys
 from django.conf import settings
+import django
 
 DIRNAME = os.path.dirname(__file__)
-settings.configure(DEBUG=True,
-                   DATABASES={
-                       'default': {
-                           'ENGINE': 'django.db.backends.sqlite3',
-                           }
-                   },
-                   ROOT_URLCONF='mailqueue.urls',
-                   INSTALLED_APPS=('django.contrib.auth',
-                                   'django.contrib.contenttypes',
-                                   'django.contrib.sessions',
-                                   'django.contrib.admin',
-                                   'mailqueue',),
-                   USE_TZ=True)
+
+if django.VERSION[1] < 4:
+    # @todo - Need to learn about settings.configure.
+
+    settings.configure(DEBUG=True,
+                       DATABASES={
+                           'default': {
+                               'ENGINE': 'django.db.backends.sqlite3',
+                               }
+                       },
+                       ROOT_URLCONF='mailqueue.urls',
+                       INSTALLED_APPS=('django.contrib.auth',
+                                       'django.contrib.contenttypes',
+                                       'django.contrib.sessions',
+                                       'django.contrib.admin',
+                                       'mailqueue',),
+                       USE_TZ=False)
+else:
+    settings.configure(DEBUG=True,
+                       DATABASES={
+                           'default': {
+                               'ENGINE': 'django.db.backends.sqlite3',
+                               }
+                       },
+                       ROOT_URLCONF='mailqueue.urls',
+                       INSTALLED_APPS=('django.contrib.auth',
+                                       'django.contrib.contenttypes',
+                                       'django.contrib.sessions',
+                                       'django.contrib.admin',
+                                       'mailqueue',),
+                       USE_TZ=True)
+
+
+
 
 from django.test.simple import DjangoTestSuiteRunner
 test_runner = DjangoTestSuiteRunner(verbosity=1)
