49929dad13bcdccb1a82400b30caf5d10f7bc2e9
Derek Stegelman <dstegelman@gmail.com>
2013-02-23 21:08:46 -0600

    Add fabric for deploying.

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,9 @@
+from fabric.api import local
+
 __author__ = 'derek'
+
+
+def deploy(version):
+    local('python runtests.py')
+    local("git tag -a %s -m %s" % (version, version))
+    local('python setup.py sdist upload')
