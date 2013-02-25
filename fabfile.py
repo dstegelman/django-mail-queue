943d2648c17facb9dbfd4f26d335beef341e9c49
Derek Stegelman <dstegelman@gmail.com>
2013-02-24 20:00:33 -0600

    Make sure to push the tags

--- fabfile.py
+++ fabfile.py
@@ -6,4 +6,5 @@ __author__ = 'derek'
 def deploy(version):
     local('python runtests.py')
     local("git tag -a %s -m %s" % (version, version))
+    local("git push origin --tags")
     local('python setup.py sdist upload')
