ee4437e9d42f8161e32d3ae0a6c739a97b5d68f4
Matt Stevens <matt@dirtymonkey.co.uk>
2013-03-27 14:24:39 +0000

    Make compatible with Celery 2.5

--- mailqueue/tasks.py
+++ mailqueue/tasks.py
@@ -1,7 +1,7 @@
-import celery
+from celery.task import task
 from .models import MailerMessage
 
-@celery.task(name="tasks.send_mail")
+@task(name="tasks.send_mail")
 def send_mail(pk):
     message = MailerMessage.objects.get(pk=pk)
     message.send()
