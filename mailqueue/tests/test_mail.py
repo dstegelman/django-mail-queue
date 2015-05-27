14b0bff86f0305d812c5fd1ad5e4cc29cc460f9d
Nielson Santana <nielsonnas@gmail.com>
2015-05-27 11:39:02 -0300

    Fix bug import utils

--- mailqueue/tests/test_mail.py
+++ mailqueue/tests/test_mail.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from mailqueue.models import MailerMessage
-from utils import create_email
+from .utils import create_email
 
 
 class MailerMessageTestCase(TestCase):
@@ -10,7 +10,7 @@ class MailerMessageTestCase(TestCase):
 
     def test_send_mail_reply_to(self):
         """Testing creating a MailerMessage and send mail"""
-        reply_to = "foo@mail.co.uk"
+        reply_to = "reply_to@mail.co.uk"
         mail = create_email(reply_to=reply_to)
         mail.send_mail()
 
