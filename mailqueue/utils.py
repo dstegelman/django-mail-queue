dba57eb869ad87b5507536b6906402d62869cd8b
Goury <goury@slayers.ru>
2016-09-14 16:35:46 +0300

    import twaeking once again

--- mailqueue/utils.py
+++ mailqueue/utils.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.utils.crypto import get_random_string
-from mailqueue.models import MailerStorage
+from .models import MailerStorage
 
 
 def get_storage():
