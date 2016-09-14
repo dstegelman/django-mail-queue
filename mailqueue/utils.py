2d9abf63c670e16252ec35d979643c316d94da06
Goury <goury@slayers.ru>
2016-09-14 03:37:00 +0300

    using MAILQUEUE_ROOT for storage when available because of security reasons

--- mailqueue/utils.py
+++ mailqueue/utils.py
@@ -1,10 +1,14 @@
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 
 def get_storage():
     if getattr(settings, 'MAILQUEUE_STORAGE', False):
-        from django.core.files.storage import FileSystemStorage
-        fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+        try:
+            location = settings.MAILQUEUE_ROOT
+        except AttributeError:
+            location = settings.MEDIA_ROOT
+        fs = FileSystemStorage(location=location)
     else:
         fs = None
     return fs
