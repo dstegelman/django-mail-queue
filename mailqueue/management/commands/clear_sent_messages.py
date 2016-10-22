f7f4f4bb26877205ab529ac0c82789b67b0face1
Derek Stegelman <dstegelman@gmail.com>
2016-10-22 07:55:30 -0600

    Fixes for management command 1.10 errors

--- mailqueue/management/commands/clear_sent_messages.py
+++ mailqueue/management/commands/clear_sent_messages.py
@@ -8,15 +8,16 @@ from mailqueue.models import MailerMessage
 class Command(BaseCommand):
     help = 'Can be run as a cronjob or directly to clean out sent messages.'
 
-    option_list = BaseCommand.option_list + (
-        make_option(
-            '--offset',
-            action='store',
-            type='int',
-            dest='offset',
-            help='Only clear messages that are more than this many hours old'
-        ),
-    )
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('offset', nargs='+', type=int)
+
+        # Named (optional) arguments
+        parser.add_argument('--offset',
+                            action='store_true',
+                            dest='offset',
+                            default=False,
+                            help='Only clear messages that are more than this many hours old')
 
     def handle(self, *args, **options):
         MailerMessage.objects.clear_sent_messages(offset=options['offset'])
